1f3066b541edc3187a6ea5eb60d359a2dac15d75
Jennifer Lin <Jennifer@dhcp-rhodes-2103.eduroam.cornell.edu>
2017-04-24 19:58:36 -0400

    Initialized project

---
+++ Lecture9/Lecture9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture9
+//
+//  Created by Jennifer Lin on 4/24/17.
+//  Copyright © 2017 org.cuappdev.lecture9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
